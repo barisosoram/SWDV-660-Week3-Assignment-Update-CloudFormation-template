bb8b736da2d0c7cbecb66ac24fcc346559c1155f
barisosoram <bsora1@live.maryville.edu>
2020-09-13 18:35:53 -0700

    final update

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Week 3 Assignment"
-   response.end('Week 3  Assignment : Update CloudFormation template\n')
+   response.end('Well Come To Week2 Class\n')
 }).listen(3333)
 
 // Console will print the message
